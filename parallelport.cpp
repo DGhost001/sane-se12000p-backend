88e2b70bd654c203c9ffdd8d4f5e7f378cefffbc
Falk 'DGhost' Ahlendorf <falk.ahlendorf@googlemail.com>
2018-01-12 22:58:33 +0100

    Added parallelport hand bit banging as the Scanner is not a fully compatible IEEE device

--- parallelport.cpp
+++ parallelport.cpp
@@ -9,11 +9,24 @@
 #include <string.h>
 #include <system_error>
 #include <unistd.h>
+#include <chrono>
+
+static void udelay(unsigned useconds)
+{
+    typedef std::chrono::duration<uint64_t, std::ratio<1,1000000> > UsDuration;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    UsDuration spawn;
+    do
+    {
+        spawn = std::chrono::duration_cast<UsDuration>(std::chrono::high_resolution_clock::now() - start);
+    }while(spawn.count()<useconds);
+}
 
 
 void ParallelPortBase::changeMode(int mode)
 {
-    execAndCheck(ioctl(fd_, PPNEGOT, &mode), "PP Modechage failed");
+    execAndCheck(ioctl(fd_, PPSETMODE, &mode), "PP Modechage failed "+std::to_string(mode));
 }
 
 void ParallelPortBase::execAndCheck(bool retValue, const std::string &message)
@@ -41,7 +54,7 @@ char ParallelPortEpp::readByte(char address)
     execAndCheck(write(fd_,&address, sizeof(address)) == sizeof(address), "Write to PP failed");
     changeMode(IEEE1284_MODE_EPP | IEEE1284_DATA);
     char result;
-    execAndCheck(read(fd_,&result, sizeof(result)) == sizeof(result), "Read from PP failed");
+    execAndCheck(read(fd_,&result, sizeof(result)) == sizeof(result), "EPP Read from PP failed");
 
     return result;
 }
@@ -49,13 +62,13 @@ char ParallelPortEpp::readByte(char address)
 void ParallelPortEpp::readString(char address, char * const buffer, size_t bufferSize)
 {
     changeMode(IEEE1284_MODE_EPP | IEEE1284_ADDR);
-    execAndCheck(write(fd_,&address, sizeof(address)) == sizeof(address), "Write to PP failed");
+    execAndCheck(write(fd_,&address, sizeof(address)) == sizeof(address), "EPP Write to PP failed");
     changeMode(IEEE1284_MODE_EPP | IEEE1284_DATA);
 
     char *bp = buffer;
     for(unsigned int i = bufferSize; i>0; --i,++bp )
     {
-        execAndCheck(read(fd_,bp, 1) == 1, "Read from PP failed");
+        execAndCheck(read(fd_,bp, 1) == 1, "EPP Read from PP failed");
     }
 }
 
@@ -65,7 +78,7 @@ void ParallelPortEpp::writeByte(char address, char byte)
     execAndCheck(write(fd_,&address, sizeof(address)) == sizeof(address), "Write to PP failed");
     changeMode(IEEE1284_MODE_EPP | IEEE1284_DATA);
 
-    execAndCheck(write(fd_,&byte, sizeof(byte)) == sizeof(byte), "Read from PP failed");
+    execAndCheck(write(fd_,&byte, sizeof(byte)) == sizeof(byte), "EPP Write to PP failed");
 }
 
 void ParallelPortEpp::writeString(char address, char const * const buffer, size_t bufferSize)
@@ -77,44 +90,172 @@ void ParallelPortEpp::writeString(char address, char const * const buffer, size_
     char const* bp = buffer;
     for(unsigned int i = bufferSize; i>0; --i,++bp )
     {
-        execAndCheck(write(fd_,bp, 1) == 1, "Read from PP failed");
+        execAndCheck(write(fd_,bp, 1) == 1, "EPP write to PP failed");
     }
 }
 
-void ParallelPortSpp::writeByte(char, char byte)
-{
-    changeMode(IEEE1284_MODE_COMPAT);
-    execAndCheck(write(fd_,&byte, sizeof(byte)) == sizeof(byte), "Read from PP failed");
 
+ParallelPortSpp::ParallelPortSpp(int fd): ParallelPortBase(fd)
+{
 }
 
-char ParallelPortSpp::readByte(char)
+void ParallelPortSpp::writeByte(char addr, char byte)
 {
+    const unsigned char  low = 0x04;
+    const unsigned char ahigh= 0x06;
+    const unsigned char dhigh= 0x05;
+
     changeMode(IEEE1284_MODE_COMPAT);
+
+    writeByte(addr);
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&low), "W-Low-1");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&ahigh), "W-High-2");
+    udelay(4);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&low), "W-Low-3");
+    udelay(1);
+    writeByte(byte);
+    udelay(4);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&dhigh), "W-High-4");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&low), "W-Low-5");
+    udelay(4);
+}
+
+char ParallelPortSpp::readByte(char addr)
+{
+
+    const unsigned char olow = 0x04;
+    const unsigned char ohigh= 0x06;
+    const unsigned char ilow = 0x04;//0x84;
+    const unsigned char ihigh= 0x05;//0x85;
+    const int data_input = 1;
+    const int data_output = 0;
+
     char result;
-    execAndCheck(read(fd_,&result, sizeof(result)) == sizeof(result), "Read from PP failed");
+
+    changeMode(IEEE1284_MODE_COMPAT);
+
+    writeByte(addr);
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&olow), "R-Low-1");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&ohigh), "R-High-2");
+    udelay(4);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&olow), "R-Low-3");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPDATADIR,&data_input), "R-Low-3");
+    execAndCheck(ioctl(fd_,PPWCONTROL,&ihigh), "R-Low-4");
+    udelay(4);
+    result = readByte();
+    execAndCheck(ioctl(fd_,PPWCONTROL,&ilow), "R-Low-5");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPDATADIR,&data_output), "R-Low-3");
+    execAndCheck(ioctl(fd_,PPWCONTROL,&olow), "R-Low-6");
+    udelay(1);
 
     return result;
 }
 
-void ParallelPortSpp::readString(char, char * const buffer, size_t bufferSize)
+void ParallelPortSpp::readString(char addr, char * const buffer, size_t bufferSize)
 {
+    const unsigned char olow = 0x04;
+    const unsigned char ohigh= 0x06;
+    const unsigned char ilow = 0x04;
+    const unsigned char ihigh= 0x05;
+
+    const int data_input = 1;
+    const int data_output = 0;
+
     changeMode(IEEE1284_MODE_COMPAT);
 
-    char *bp = buffer;
-    for(unsigned int i = bufferSize; i>0; --i,++bp )
+    writeByte(addr);
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&olow), "R-Low-1");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&ohigh), "R-High-2");
+    udelay(4);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&olow), "R-Low-3");
+    udelay(1);
+
+    execAndCheck(ioctl(fd_,PPDATADIR,&data_input), "R-Low-3");
+
+    for(size_t i = 0; i<bufferSize; ++i)
     {
-        execAndCheck(read(fd_,bp, 1) == 1, "Read from PP failed");
+        execAndCheck(ioctl(fd_,PPWCONTROL,&ihigh), "R-Low-4");
+        udelay(1);
+        buffer[i] = readByte();
+        execAndCheck(ioctl(fd_,PPWCONTROL,&ilow), "R-Low-5");
+        udelay(1);
+    }
+
+    execAndCheck(ioctl(fd_,PPDATADIR,&data_output), "R-Low-3");
+
+    execAndCheck(ioctl(fd_,PPWCONTROL,&olow), "R-Low-6");
+    udelay(1);
+}
+
+void ParallelPortSpp::writeString(char addr, char const*const buffer, size_t bufferSize)
+{
+    const unsigned char low = 0x04;
+    const unsigned char ahigh= 0x06;
+    const unsigned char dhigh= 0x06;
+
+    changeMode(IEEE1284_MODE_COMPAT);
+
+    writeByte(addr);
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&low), "W-Low-1");
+    udelay(1);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&ahigh), "W-High-2");
+    udelay(4);
+    execAndCheck(ioctl(fd_,PPWCONTROL,&low), "W-Low-3");
+    udelay(1);
+
+    for(size_t i = 0; i<bufferSize; ++i )
+    {
+        writeByte(buffer[i]);
+        udelay(4);
+        execAndCheck(ioctl(fd_,PPWCONTROL,&dhigh), "W-High-4");
+        udelay(1);
+        execAndCheck(ioctl(fd_,PPWCONTROL,&low), "W-Low-5");
+        udelay(4);
     }
 }
 
-void ParallelPortSpp::writeString(char, char const*const buffer, size_t bufferSize)
+void  ParallelPortSpp::writeByte(char byte)
 {
-    changeMode(IEEE1284_MODE_COMPAT);
-    char const* bp = buffer;
-    for(unsigned int i = bufferSize; i>0; --i,++bp )
-    {
-        execAndCheck(write(fd_,bp, 1) == 1, "Read from PP failed");
-    }
+    const unsigned char c = static_cast<unsigned char>(byte);
 
+    changeMode(IEEE1284_MODE_COMPAT);
+    execAndCheck(ioctl(fd_,PPWDATA,&c), "Write to PP failed");
 }
+
+char  ParallelPortSpp::readByte()
+{
+    unsigned char c = 0;
+
+    changeMode(IEEE1284_MODE_COMPAT);
+
+    execAndCheck(ioctl(fd_,PPRDATA,&c), "Write to PP failed");
+
+    return c;
+}
+
+void  ParallelPortSpp::readString( char * const buffer, size_t bufferSize)
+{
+    for(size_t i=0; i<bufferSize; ++i)
+    {
+        buffer[i] = readByte();
+    }
+}
+
+void  ParallelPortSpp::writeString( char const*const buffer, size_t bufferSize)
+{
+    for(size_t i=0; i<bufferSize; ++i)
+    {
+        writeByte(buffer[i]);
+    }
+}
+
--- parallelport.hpp
+++ parallelport.hpp
@@ -15,9 +15,16 @@ public:
     virtual char readByte(char address) = 0;
     virtual void readString(char address, char * const buffer, size_t bufferSize) = 0;
     virtual void writeString(char address, char const*const buffer, size_t bfferSize) = 0;
+
+    virtual void writeByte(char byte) = 0;
+    virtual char readByte() = 0;
+    virtual void readString( char * const buffer, size_t bufferSize) = 0;
+    virtual void writeString( char const*const buffer, size_t bfferSize) = 0;
+
+    void changeMode(int mode);
+
 protected:
     int fd_;
-    void changeMode(int mode);
     void execAndCheck(int retValue, const std::string &message);
     void execAndCheck(bool retValue, const std::string &message);
 };
@@ -42,10 +49,16 @@ class ParallelPortSpp: public ParallelPortBase
 public:
     ParallelPortSpp(int fd);
 
-    virtual void writeByte(char, char byte);
+    virtual void writeByte(char addr, char byte);
     virtual char readByte(char);
     virtual void readString(char, char * const buffer, size_t bufferSize);
     virtual void writeString(char address, char const*const buffer, size_t bufferSize);
+
+    virtual void writeByte(char byte);
+    virtual char readByte();
+    virtual void readString( char * const buffer, size_t bufferSize);
+    virtual void writeString(char const*const buffer, size_t bufferSize);
+
 };
 
 #endif // PARALLELPORT_H
